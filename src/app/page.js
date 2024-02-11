e0e7fddeb5e4ea57f90e3486705618a05ab26b86
Imani A <22506762+Imanisima@users.noreply.github.com>
2024-02-11 00:48:22 -0600

    updating server email

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,16 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ComingSoon from './components/ComingSoon';
 
-import { TextInput, Textarea, Button } from "@mantine/core";
-
 export default function Home() {
   return (
     <div>
       <Navbar />
-      
-      <main className="flex max-h-screen flex-col items-center justify-between p-10">
-        <ComingSoon />
-        
+
+      <main className="min-h-screen bg-[#121212]">
+        <div className='overflow-hidden'>
+          <ComingSoon />
+        </div>
+
       </main>
 
       <Footer />
